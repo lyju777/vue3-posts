fb8b3045c66aa7c08338e836efee1bf9b6f6a313
lyju777 <lyju777@hanmail.net>
2023-12-20 21:40:59 +0900

    Teleport 컴포넌트 : Modal 만들기

--- src/components/posts/PostItem.vue
+++ src/components/posts/PostItem.vue
@@ -5,6 +5,13 @@
 			{{ content }}
 		</p>
 		<p class="text-muted">{{ createdAt }}</p>
+		<template #footer>
+			<div class="d-flex flex-row-reverse">
+				<button class="btn p-0" @click.stop="$emit('modal')">
+					<i class="bi bi-emoji-sunglasses"></i>
+				</button>
+			</div>
+		</template>
 	</AppCard>
 </template>
 
@@ -23,6 +30,8 @@ defineProps({
 		type: [String, Date, Number],
 	},
 });
+
+defineEmits(['modal']);
 </script>
 
 <style lang="scss" scoped></style>
--- src/views/posts/PostListView.vue
+++ src/views/posts/PostListView.vue
@@ -15,6 +15,7 @@
 					:content="item.content"
 					:createdAt="item.createdAt"
 					@click="goPage(item.id)"
+					@modal="openModal(item)"
 				></PostItem>
 			</template>
 		</AppGrid>
@@ -23,6 +24,15 @@
 			:page-count="pageCount"
 			@page="page => (params._page = page)"
 		/>
+		<Teleport to="#modal">
+			<postModal
+				v-model="show"
+				:title="modalTitle"
+				:content="modalContent"
+				:created-at="modalCreatedAt"
+			/>
+		</Teleport>
+
 		<template v-if="posts && posts.length > 0">
 			<hr class="my-5" />
 			<AppCard>
@@ -37,6 +47,7 @@ import PostItem from '@/components/posts/PostItem.vue';
 import PostDetailView from '@/views/posts/PostDetailView.vue';
 import AppPageination from '@/components/AppPageination.vue';
 import postFilter from '@/components/posts/postFilter.vue';
+import postModal from '@/components/posts/postModal.vue';
 import AppGrid from '@/components/AppGrid.vue';
 import AppCard from '@/components/AppCard.vue';
 import { getPosts } from '@/api/posts';
@@ -84,6 +95,18 @@ const goPage = id => {
 		},
 		hash: '#wrold',
 	});
+};
+
+//model
+const show = ref(false);
+const modalTitle = ref('');
+const modalContent = ref('');
+const modalCreatedAt = ref('');
+const openModal = ({ title, content, createdAt }) => {
+	show.value = true;
+	modalTitle.value = title;
+	modalContent.value = content;
+	modalCreatedAt.value = createdAt;
 };
 </script>
 
